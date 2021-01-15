5a412a548c94a326199f56192253d7a71410f2de
engineerscodes <practicalengineers05@gmail.com>
2021-01-15 20:10:23 +0530

    sorting added

--- DataStructure/src/Queues/Queue_Arrays.java
+++ DataStructure/src/Queues/Queue_Arrays.java
@@ -32,15 +32,52 @@ public class Queue_Arrays
 	     queues[++rear]=data;
 	     System.out.println("The value is Inserted :"+queues[rear]+" :at -"+rear); 	 
 	}
+	  public void pop() throws emptyQueueException
+	    {
+	    	if(rear==-1 && front ==-1) {
+	    		throw new emptyQueueException();
+	    	}
+	    	if(rear==front) {System.out.println("the element is dequeued :"+queues[front]);
+	    	   rear=front=-1;
+	    	return;}
+	    	System.out.println("the element is dequeued :"+queues[front]);
+	    	front++;
+	    }
+	  public void sort ()
+	    {  System.out.println("!!!!!!!!!!!!!!!!!Sorting !!!!!!!!!!!!!!!!!");
+	  	  ArrayList <Integer>temp=new ArrayList();
+	  	  for(Integer i :queues) {temp.add(i);}
+	  	  temp.sort(null);
+	  	  System.out.println(temp);
+	  	  for(int i=front;i<=rear;i++) {queues[i]=temp.get(i);}
+	  	  
+	    }
+	    public String toString()
+	    {   System.out.println("!!!!!!!!!!!!!!!!!!!!! Element's in Queue !!!!!!!!!!!!!!!!!!!");
+	       if(front ==-1&& rear==-1) {System.out.println("Empty Queue"); return"";}
+	        
+	    	for(int i=front ;i<=rear;i++)
+	    	{
+	    		System.out.println(queues[i ]);
+	    	}
+	    	return "!!!!!!!!!!!!!!!!!!!!!!!End of the Queue!!!!!!!!!!!!!!!!!!!!!";
+	    }
+	    
+	  
     public static void main(String ...strings) throws queueFullException,emptyQueueException
     {
     	Queue_Arrays qa=new Queue_Arrays(5);
     	qa.Inqueue(1);
-    	qa.Inqueue(3);
-    	qa.Inqueue(9);
-    	qa.Inqueue(5);
+    	qa.Inqueue(39);
+    	qa.Inqueue(99);
+    	qa.Inqueue(58);
     	qa.Inqueue(8);
+    	
     	System.out.println(qa);
+    	qa.sort();
+    	System.out.println("After sorting !!!!!");
+    	System.out.println(qa);
+    	
     	qa.pop();
     	qa.pop();
     	qa.pop();
@@ -48,29 +85,7 @@ public class Queue_Arrays
     	qa.pop();
     	System.out.println(qa);
     }
-    
-    public String toString()
-    {   System.out.println("!!!!!!!!!!!!!!!!!!!!! Element's in Queue !!!!!!!!!!!!!!!!!!!");
-       if(front ==-1&& rear==-1) {System.out.println("Empty Queue"); return"";}
-        
-    	for(int i=front ;i<=rear;i++)
-    	{
-    		System.out.println(queues[i ]);
-    	}
-    	return "!!!!!!!!!!!!!!!!!!!!!!!End of the Queue!!!!!!!!!!!!!!!!!!!!!";
-    }
-    
-    public void pop() throws emptyQueueException
-    {
-    	if(rear==-1 && front ==-1) {
-    		throw new emptyQueueException();
-    	}
-    	if(rear==front) {System.out.println("the element is dequeued :"+queues[front]);
-    	   rear=front=-1;
-    	return;}
-    	System.out.println("the element is dequeued :"+queues[front]);
-    	front++;
-    }
+  
 }
 class emptyQueueException extends Exception
 {
