73ab9cdde7b5967729b8d7a7fca6fbb479ad330d
engineerscodes <practicalengineers05@gmail.com>
2021-01-15 23:59:26 +0530

    added dequeue

--- DataStructure/src/Queues/Queue_generics.java
+++ DataStructure/src/Queues/Queue_generics.java
@@ -43,25 +43,50 @@ public class Queue_generics <T>
 			
 	}
 	
-	public String toString()
+	public void deQueue() throws emptyQueueException
 	{
+		if(front==null && rear==null) {
+			throw new emptyQueueException();
+		}
+		if(front==rear) {
+			System.out.println("THE ELEMENT IS POPED WITH VALUE : "+front.data);
+			front=null;
+			rear=null;
+			return ;
+		}
+		System.out.println("THE ELEMENT IS POPED WITH VALUE : "+front.data);
+		front=front.next;
+	}
+	
+	public String toString()
+	{   System.out.println("---------------------------Element in QUEUE----------------------------------------------------");
+	    if(front==null)
+	    {
+	    	System.out.println("QUEUE IS EMPTY !!!!!INSERT VALUES");
+	    	return "";
+	    }
 		block<T> temp=front;
 		while(temp!=null)
 		{
-			System.out.println("Value <-:->"+temp.data);
+			System.out.println("Value : "+temp.data);
 			temp=temp.next;
 		}
-		return "---------------------------Printing Done-----------------------------------------------------";
+		return "";
 	}
-	public static void main(String ...args)
+	
+	
+	public static void main(String ...args)throws emptyQueueException
 	{
 		
 		Queue_generics <Integer> qg=new Queue_generics();
 		qg.Queue(5);
 		System.out.println(qg);
+		qg.deQueue();
+		System.out.println(qg);
 		qg.Queue(100);
 		qg.Queue(11);
 		System.out.println(qg);
+		
 		Queue_generics<Character> q=new Queue_generics<Character>();
 		Queue_generics<Double> q2=new Queue_generics<>();
 		for (int i=0;i<26;i++)
@@ -71,7 +96,12 @@ public class Queue_generics <T>
 		}
 		System.out.println(q);
 		System.out.println(q2);
-		
+		System.out.println("Poping Out values ");
+		for (int i=0;i<20;i++)
+		{
+			q.deQueue();
+		}
+		System.out.println(q);
 	}
 	
 }
