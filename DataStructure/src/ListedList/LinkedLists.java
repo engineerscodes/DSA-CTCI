9afb3ae138f7965de134ec73ab7a48f4b437be0b
engineerscodes <practicalengineers05@gmail.com>
2021-01-17 22:09:50 +0530

    Setup

---
+++ DataStructure/src/ListedList/LinkedLists.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ListedList;
+import java.util.*;
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class LinkedLists 
+{
+    node head;
+	static class node
+	{
+		 
+	}
+	public LinkedLists() 
+	{
+	
+	}
+   
+}
+
