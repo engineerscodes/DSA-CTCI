3c4d6fa99e6980d24876b92b12d73a80588d1696
engineerscodes <practicalengineers05@gmail.com>
2021-07-01 12:55:49 +0530

    initial commit

---
+++ DataStructure/src/RoadMap/Arrays/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+package RoadMap.Arrays;
+
+//Array Questions
+
+/*
+In Java all arrays are dynamically allocated.(discussed below)
+Since arrays are objects in Java, we can find their length using the object property length. This is different from C/C++ where we find length using sizeof.
+A Java array variable can also be declared like other variables with [] after the data type.
+The variables in the array are ordered and each have an index beginning from 0.
+Java array can be also be used as a static field, a local variable or a method parameter.
+The size of an array must be specified by an int or short value and not long.
+The direct superclass of an array type is Object.
+Every array type implements the interfaces Cloneable and java.io.Serializable.
+ */
