8207fda7ddbe71c986486d891f73b108ff6464f7
engineerscodes <practicalengineers05@gmail.com>
2021-07-01 13:08:05 +0530

    basic

---
+++ DataStructure/src/RoadMap/Arrays/Array_basic.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package RoadMap.Arrays;
+import java.util.*;
+
+import static java.lang.System.out;
+
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class Array_basic {
+
+	public Array_basic() {
+		
+		
+	}
+   
+	public static void main(String ...strings) {
+		
+		out.println("Array Demo !");
+		
+	}
+	
+}
+
