3e20ca8da6182e0be7c1ea8452f1c85314df27af
engineerscodes <practicalengineers05@gmail.com>
2021-07-10 18:47:09 +0530

    initial Commit

---
+++ DataStructure/src/RoadMap/Arrays/MinMax.java
@@ -0,0 +1,24 @@
+/**
+Program to find the minimum (or maximum) element of an array
+Difficulty Level : Easy
+Last Updated : 07 May, 2021
+Given an array, write functions to find the minimum and maximum elements in it.
+ */
+package RoadMap.Arrays;
+
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class MinMax {
+
+	/**
+	 * 
+	 */
+	public MinMax() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
