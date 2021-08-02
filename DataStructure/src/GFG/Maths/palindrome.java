f9ea25e53da15f56fcb9a43ab11597dd01353d39
engineerscodes <practicalengineers05@gmail.com>
2021-08-02 18:26:08 +0530

    initial commit

---
+++ DataStructure/src/GFG/Maths/palindrome.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package GFG.Maths;
+
+/**
+ * @author M.NAVEEN
+ * RANDOM CODER'S
+ *
+ */
+public class palindrome {
+
+	public palindrome() {
+	
+	}
+    
+	public static void main(String ...strings) 
+	{
+		
+		
+		
+		
+	}
+}
