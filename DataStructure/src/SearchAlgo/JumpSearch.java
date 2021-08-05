0ba18cf0c1204904d5e538ed5c1e77001b4894c8
engineerscodes <practicalengineers05@gmail.com>
2021-08-05 19:28:22 +0530

    initial Commit

---
+++ DataStructure/src/SearchAlgo/JumpSearch.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package SearchAlgo;
+
+/**
+ * @author M.NAVEEN
+ * RANDOM CODER'S
+ *
+ */
+public class JumpSearch {
+
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+}
